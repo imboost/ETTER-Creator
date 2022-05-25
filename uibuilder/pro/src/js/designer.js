bbe8b03b432f16fa5dc30a0058d029a46128e138
Taufik Sulaeman P <taufiksu@gmail.com>
2022-05-25 10:22:50 +0700

    1. Form item stack

--- uibuilder/pro/src/js/designer.js
+++ uibuilder/pro/src/js/designer.js
@@ -22273,3 +22273,288 @@ $$(document).on('click', '.btn-element-add-form-inline-item-textearea-grow', fun
         });
     }
 });
+
+$$(document).on('click', '.btn-element-add-form-stack-item-text', function () {
+    var action = $$(this).attr('data-action');
+
+    if (element_id_selected === null) {
+        app.dialog.alert('Choose element to modify!');
+    } else {
+        uibuilder.send({
+            "topic": "add_element_component",
+            "element_id": element_id_selected,
+            "component": "<li class=\"item-content item-input\">\n" +
+                "\t<div class=\"item-media\">\n" +
+                "\t\t<i class=\"icon f7-icons\">pencil_ellipsis_rectangle</i>\n" +
+                "\t</div>\n" +
+                "\t<div class=\"item-inner\">\n" +
+                "\t\t<div class=\"item-title item-label\">Name</div>\n" +
+                "\t\t<div class=\"item-input-wrap\">\n" +
+                "\t\t\t<input type=\"text\" placeholder=\"Your name\" />\n" +
+                "\t\t\t<span class=\"input-clear-button\"></span>\n" +
+                "\t\t</div>\n" +
+                "\t</div>\n" +
+                "</li>\n",
+            "action": action
+        });
+    }
+});
+
+$$(document).on('click', '.btn-element-add-form-stack-item-password', function () {
+    var action = $$(this).attr('data-action');
+
+    if (element_id_selected === null) {
+        app.dialog.alert('Choose element to modify!');
+    } else {
+        uibuilder.send({
+            "topic": "add_element_component",
+            "element_id": element_id_selected,
+            "component": "<li class=\"item-content item-input\">\n" +
+                "\t<div class=\"item-media\">\n" +
+                "\t\t<i class=\"icon f7-icons\">pencil_ellipsis_rectangle</i>\n" +
+                "\t</div>\n" +
+                "\t<div class=\"item-inner\">\n" +
+                "\t\t<div class=\"item-title item-label\">Password</div>\n" +
+                "\t\t<div class=\"item-input-wrap\">\n" +
+                "\t\t\t<input type=\"password\" placeholder=\"Your password\" />\n" +
+                "\t\t\t<span class=\"input-clear-button\"></span>\n" +
+                "\t\t</div>\n" +
+                "\t</div>\n" +
+                "</li>\n",
+            "action": action
+        });
+    }
+});
+
+$$(document).on('click', '.btn-element-add-form-stack-item-email', function () {
+    var action = $$(this).attr('data-action');
+
+    if (element_id_selected === null) {
+        app.dialog.alert('Choose element to modify!');
+    } else {
+        uibuilder.send({
+            "topic": "add_element_component",
+            "element_id": element_id_selected,
+            "component": "<li class=\"item-content item-input\">\n" +
+                "\t<div class=\"item-media\">\n" +
+                "\t\t<i class=\"icon f7-icons\">pencil_ellipsis_rectangle</i>\n" +
+                "\t</div>\n" +
+                "\t<div class=\"item-inner\">\n" +
+                "\t\t<div class=\"item-title item-label\">E-mail</div>\n" +
+                "\t\t<div class=\"item-input-wrap\">\n" +
+                "\t\t\t<input type=\"email\" placeholder=\"Your e-mail\" />\n" +
+                "\t\t\t<span class=\"input-clear-button\"></span>\n" +
+                "\t\t</div>\n" +
+                "\t</div>\n" +
+                "</li>\n",
+            "action": action
+        });
+    }
+});
+
+$$(document).on('click', '.btn-element-add-form-stack-item-url', function () {
+    var action = $$(this).attr('data-action');
+
+    if (element_id_selected === null) {
+        app.dialog.alert('Choose element to modify!');
+    } else {
+        uibuilder.send({
+            "topic": "add_element_component",
+            "element_id": element_id_selected,
+            "component": "<li class=\"item-content item-input\">\n" +
+                "\t<div class=\"item-media\">\n" +
+                "\t\t<i class=\"icon f7-icons\">pencil_ellipsis_rectangle</i>\n" +
+                "\t</div>\n" +
+                "\t<div class=\"item-inner\">\n" +
+                "\t\t<div class=\"item-title item-label\">URL</div>\n" +
+                "\t\t<div class=\"item-input-wrap\">\n" +
+                "\t\t\t<input type=\"url\" placeholder=\"URL\" />\n" +
+                "\t\t\t<span class=\"input-clear-button\"></span>\n" +
+                "\t\t</div>\n" +
+                "\t</div>\n" +
+                "</li>\n",
+            "action": action
+        });
+    }
+});
+
+$$(document).on('click', '.btn-element-add-form-stack-item-phone', function () {
+    var action = $$(this).attr('data-action');
+
+    if (element_id_selected === null) {
+        app.dialog.alert('Choose element to modify!');
+    } else {
+        uibuilder.send({
+            "topic": "add_element_component",
+            "element_id": element_id_selected,
+            "component": "<li class=\"item-content item-input\">\n" +
+                "\t<div class=\"item-media\">\n" +
+                "\t\t<i class=\"icon f7-icons\">pencil_ellipsis_rectangle</i>\n" +
+                "\t</div>\n" +
+                "\t<div class=\"item-inner\">\n" +
+                "\t\t<div class=\"item-title item-label\">Phone</div>\n" +
+                "\t\t<div class=\"item-input-wrap\">\n" +
+                "\t\t\t<input type=\"tel\" placeholder=\"Your phone number\" />\n" +
+                "\t\t\t<span class=\"input-clear-button\"></span>\n" +
+                "\t\t</div>\n" +
+                "\t</div>\n" +
+                "</li>\n",
+            "action": action
+        });
+    }
+});
+
+$$(document).on('click', '.btn-element-add-form-stack-item-option', function () {
+    var action = $$(this).attr('data-action');
+
+    if (element_id_selected === null) {
+        app.dialog.alert('Choose element to modify!');
+    } else {
+        uibuilder.send({
+            "topic": "add_element_component",
+            "element_id": element_id_selected,
+            "component": "<li class=\"item-content item-input\">\n" +
+                "\t<div class=\"item-media\">\n" +
+                "\t\t<i class=\"icon f7-icons\">pencil_ellipsis_rectangle</i>\n" +
+                "\t</div>\n" +
+                "\t<div class=\"item-inner\">\n" +
+                "\t\t<div class=\"item-title item-label\">Gender</div>\n" +
+                "\t\t<div class=\"item-input-wrap input-dropdown-wrap\">\n" +
+                "\t\t\t<select placeholder=\"Please choose...\">\n" +
+                "\t\t\t\t<option value=\"Male\">Male</option>\n" +
+                "\t\t\t\t<option value=\"Female\">Female</option>\n" +
+                "\t\t\t</select>\n" +
+                "\t\t</div>\n" +
+                "\t</div>\n" +
+                "</li>\n",
+            "action": action
+        });
+    }
+});
+
+$$(document).on('click', '.btn-element-add-form-stack-item-date', function () {
+    var action = $$(this).attr('data-action');
+
+    if (element_id_selected === null) {
+        app.dialog.alert('Choose element to modify!');
+    } else {
+        uibuilder.send({
+            "topic": "add_element_component",
+            "element_id": element_id_selected,
+            "component": "<li class=\"item-content item-input\">\n" +
+                "\t<div class=\"item-media\">\n" +
+                "\t\t<i class=\"icon f7-icons\">pencil_ellipsis_rectangle</i>\n" +
+                "\t</div>\n" +
+                "\t<div class=\"item-inner\">\n" +
+                "\t\t<div class=\"item-title item-label\">Birthday</div>\n" +
+                "\t\t<div class=\"item-input-wrap\">\n" +
+                "\t\t\t<input type=\"date\" value=\"2014-04-30\" placeholder=\"Please choose...\" />\n" +
+                "\t\t</div>\n" +
+                "\t</div>\n" +
+                "</li>\n",
+            "action": action
+        });
+    }
+});
+
+$$(document).on('click', '.btn-element-add-form-stack-item-datetime', function () {
+    var action = $$(this).attr('data-action');
+
+    if (element_id_selected === null) {
+        app.dialog.alert('Choose element to modify!');
+    } else {
+        uibuilder.send({
+            "topic": "add_element_component",
+            "element_id": element_id_selected,
+            "component": "<li class=\"item-content item-input\">\n" +
+                "\t<div class=\"item-media\">\n" +
+                "\t\t<i class=\"icon f7-icons\">pencil_ellipsis_rectangle</i>\n" +
+                "\t</div>\n" +
+                "\t<div class=\"item-inner\">\n" +
+                "\t\t<div class=\"item-title item-label\">Date time</div>\n" +
+                "\t\t<div class=\"item-input-wrap\">\n" +
+                "\t\t\t<input type=\"datetime-local\" placeholder=\"Please choose...\" />\n" +
+                "\t\t</div>\n" +
+                "\t</div>\n" +
+                "</li>\n",
+            "action": action
+        });
+    }
+});
+
+$$(document).on('click', '.btn-element-add-form-stack-item-range', function () {
+    var action = $$(this).attr('data-action');
+
+    if (element_id_selected === null) {
+        app.dialog.alert('Choose element to modify!');
+    } else {
+        uibuilder.send({
+            "topic": "add_element_component",
+            "element_id": element_id_selected,
+            "component": "<li class=\"item-content item-input\">\n" +
+                "\t<div class=\"item-media\">\n" +
+                "\t\t<i class=\"icon f7-icons\">pencil_ellipsis_rectangle</i>\n" +
+                "\t</div>\n" +
+                "\t<div class=\"item-inner\">\n" +
+                "\t\t<div class=\"item-title item-label\">Range</div>\n" +
+                "\t\t<div class=\"item-input-wrap\">\n" +
+                "\t\t\t<div class=\"range-slider range-slider-init\" data-label=\"true\">\n" +
+                "\t\t\t\t<input type=\"range\" value=\"50\" min=\"0\" max=\"100\" step=\"1\" />\n" +
+                "\t\t\t</div>\n" +
+                "\t\t</div>\n" +
+                "\t</div>\n" +
+                "</li>\n",
+            "action": action
+        });
+    }
+});
+
+$$(document).on('click', '.btn-element-add-form-stack-item-textarea', function () {
+    var action = $$(this).attr('data-action');
+
+    if (element_id_selected === null) {
+        app.dialog.alert('Choose element to modify!');
+    } else {
+        uibuilder.send({
+            "topic": "add_element_component",
+            "element_id": element_id_selected,
+            "component": "<li class=\"item-content item-input\">\n" +
+                "\t<div class=\"item-media\">\n" +
+                "\t\t<i class=\"icon f7-icons\">pencil_ellipsis_rectangle</i>\n" +
+                "\t</div>\n" +
+                "\t<div class=\"item-inner\">\n" +
+                "\t\t<div class=\"item-title item-label\">Textarea</div>\n" +
+                "\t\t<div class=\"item-input-wrap\">\n" +
+                "\t\t\t<textarea placeholder=\"Bio\"></textarea>\n" +
+                "\t\t</div>\n" +
+                "\t</div>\n" +
+                "</li>\n",
+            "action": action
+        });
+    }
+});
+
+$$(document).on('click', '.btn-element-add-form-stack-item-textarea-grow', function () {
+    var action = $$(this).attr('data-action');
+
+    if (element_id_selected === null) {
+        app.dialog.alert('Choose element to modify!');
+    } else {
+        uibuilder.send({
+            "topic": "add_element_component",
+            "element_id": element_id_selected,
+            "component": "<li class=\"item-content item-input\">\n" +
+                "\t<div class=\"item-media\">\n" +
+                "\t\t<i class=\"icon f7-icons\">pencil_ellipsis_rectangle</i>\n" +
+                "\t</div>\n" +
+                "\t<div class=\"item-inner\">\n" +
+                "\t\t<div class=\"item-title item-label\">Resizable</div>\n" +
+                "\t\t<div class=\"item-input-wrap\">\n" +
+                "\t\t\t<textarea class=\"resizable\" placeholder=\"Bio\"></textarea>\n" +
+                "\t\t</div>\n" +
+                "\t</div>\n" +
+                "</li>\n",
+            "action": action
+        });
+    }
+});
