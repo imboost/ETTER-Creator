8a7b8fba16f46292dc95f39c90b80325285d3bfe
Taufik Sulaeman P <taufiksu@gmail.com>
2022-11-27 05:53:09 +0700

    1. Remove node_modules

--- node_modules/weak-daemon/lib/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { WeakDaemon } from './weak-daemon';
-declare function getInstance(interval_time: number, caller: object | null, task: (...args: any[]) => any, task_arguments?: any[]): WeakDaemon;
-declare function getClass(): typeof WeakDaemon;
-export { getInstance };
-export { getClass };
-export { WeakDaemon };
